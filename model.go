7647536b2fc5e0af94e3208a0a4dec2fb8cf9e2e
daghassi <79544966+daghassi@users.noreply.github.com>
2024-10-13 04:21:40 -0600

    upvotes fix attempted

--- model.go
+++ model.go
@@ -20,14 +20,13 @@ var (
 type Post struct {
 	Title     string
 	Rank      int
-	RankDelta int
+	RankDelta int // This will be used to track the user's vote
 	Username  string
 }
 
 func (p Post) FilterValue() string { return p.Title }
 
-type postDelegate struct {
-}
+type postDelegate struct{}
 
 func (d postDelegate) Height() int                               { return 1 }
 func (d postDelegate) Spacing() int                              { return 0 }
@@ -50,8 +49,7 @@ func (d postDelegate) Render(w io.Writer, m list.Model, index int, listItem list
 		fn = selectedItemStyle.Render
 	}
 
-	fmt.Fprintln(w, fmt.Sprintf("   ▲ %s ▼ %s \n", postCount, fn(post.Username + " - " + post.Title)))
-
+	fmt.Fprintln(w, fmt.Sprintf("   ▲ %s ▼ %s \n", postCount, fn(post.Username+" - "+post.Title)))
 }
 
 // Just a generic tea.Model to demo terminal information of ssh.
@@ -81,11 +79,32 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.list.SetWidth(msg.Width)
 		m.list.SetHeight(msg.Height - 8)
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "+", "=":
-		case "-", "_":
-		case "q", "ctrl+c":
-			return m, tea.Quit
+		index := m.list.Index()
+		if index >= 0 {
+			if item, ok := m.list.SelectedItem().(Post); ok {
+				switch msg.String() {
+				case "up": // Upvote
+					if item.RankDelta == -1 { // User previously downvoted
+						item.Rank++   // Remove the downvote
+					}
+					if item.RankDelta != 1 { // User has not upvoted yet
+						item.Rank++       // Add upvote
+						item.RankDelta = 1 // Mark as upvoted
+					}
+					m.list.SetItem(index, item) // Update the selected item
+				case "down": // Downvote
+					if item.RankDelta == 1 { // User previously upvoted
+						item.Rank--   // Remove the upvote
+					}
+					if item.RankDelta != -1 { // User has not downvoted yet
+						item.Rank--       // Add downvote
+						item.RankDelta = -1 // Mark as downvoted
+					}
+					m.list.SetItem(index, item) // Update the selected item
+				case "q", "ctrl+c":
+					return m, tea.Quit
+				}
+			}
 		}
 	}
 
@@ -97,8 +116,6 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-
 	list := m.list.View()
-
 	return list
 }
